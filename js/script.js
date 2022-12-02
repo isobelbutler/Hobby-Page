59039d8c32a55c62790b131107867c11dd173f04
isobelbutler <isobutler@icloud.com>
2022-12-02 21:52:27 +0000

    + if clause to date for styling time

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-var date = new Date();
-document.getElementById("clock").innerHTML = date.toLocaleString();
+const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1; // You need + 1 as month is seen as 0-11.
+let year = date.getFullYear();
+let headingDate = `${day} / ${month} / ${year}`;
+
+
+// This adds a 0 prefix if day/month is a single digit.
+if ( day <= 9 & month >= 10) {
+    headingDate = `0${day} / ${month} / ${year}`;
+}  else if ( day >= 10 & month <= 9 ) {
+    headingDate = `${day} / 0${month} / ${year}`;
+}  else if ( day <= 9 & month <= 9) {
+    headingDate = `0${day} / 0${month} / ${year}`;
+}  
+
+document.getElementById("clock").innerHTML = headingDate.toLocaleString();
