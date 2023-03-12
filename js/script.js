e38c4a7bdc37317b25e8590bf6efc7208f6802e0
isobelbutler <isobutler@icloud.com>
2023-03-12 21:56:47 +0000

    added smooth scrolling

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ let month = date.getMonth() + 1; // You need + 1 as month is seen as 0-11.
 let year = date.getFullYear();
 let headingDate = `${day} / ${month} / ${year}`;
 
-
 // This adds a 0 prefix if day/month is a single digit - Aesthetic Only
 if ( day <= 9 & month >= 10) {
     headingDate = `0${day} / ${month} / ${year}`;
