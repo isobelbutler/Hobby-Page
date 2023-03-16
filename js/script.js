23c5fa6789dc370e21cb6d9d678dbdbbd72665ee
isobelbutler <isobutler@icloud.com>
2023-03-16 09:37:28 +0000

    tidying code tweaks

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
+// Variables
+
 const date = new Date();
 
 let day = date.getDate();
-let month = date.getMonth() + 1; // You need + 1 as month is seen as 0-11.
+let month = date.getMonth() + 1; // you need + 1 as month is seen as 0-11.
 let year = date.getFullYear();
-let headingDate = `${day} / ${month} / ${year}`;
+let headingDate = `${day} / ${month} / ${year}`; // string interpolation
 
-// This adds a 0 prefix if day/month is a single digit - Aesthetic Only
+// Conditions
+
+    // this adds a 0 prefix if day/month is a single digit - aesthetic Only
 if ( day <= 9 & month >= 10) {
     headingDate = `0${day} / ${month} / ${year}`;
 }  else if ( day >= 10 & month <= 9 ) {
